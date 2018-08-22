c94622b829e037d1014b22af0f1d070f96833600
Davaeth <masterbelki@gmail.com>
2018-08-22 23:52:29 +0200

    Add some new txt files, class and methos for future calculator system.

---
+++ WWBot/Data/Materials/Classes/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WWBot.Data.Materials.Classes
+{
+    public class Card
+    {
+        public List<int> Gold { get; set; }
+        public List<int> Materials { get; set; }
+        public List<int> Crystals { get; set; }
+
+        public Card ()
+        {
+            this.Gold = new List<int>();
+            this.Materials = new List<int>();
+            this.Crystals = new List<int>();
+        }
+    }
+}
