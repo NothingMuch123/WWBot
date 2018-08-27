aeb44e9c78f432a4a8360d784f8db173e68cee51
Davaeth <masterbelki@gmail.com>
2018-08-27 11:46:35 +0200

    -Added a file reader for materials -Tried to add some commands for calculator

---
+++ WWBot/Data/Materials/Classes/Monster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WWBot.Data.Materials.Classes
+{
+    public class Monster : ICard
+    {
+        public int gold { get; set; }
+        public int materials { get; set; }
+        public int crystals { get; set; }
+        public int diamonds { get; set; }
+
+        public Monster(int gold, int materials, int crystals, int diamonds = 0)
+        {
+            this.gold = gold;
+            this.materials = materials;
+            this.crystals = crystals;
+            this.diamonds = diamonds;
+        }
+
+    }
+}
---
+++ WWBot/bin/Debug/netcoreapp2.0/Data/Materials/Classes/Gear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WWBot.Data.Materials.Classes
+{
+    class Gear : ICard
+    {
+        public int gold { get; set; }
+        public int materials { get; set; }
+        public int crystals { get; set; }
+
+        public Gear(int gold, int materials, int crystals)
+        {
+            this.gold = gold;
+            this.materials = materials;
+            this.crystals = crystals;
+        }
+    }
+}
